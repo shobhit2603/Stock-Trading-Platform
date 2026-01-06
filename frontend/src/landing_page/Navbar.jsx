cace80007a360bcaac834d79de18ebe8ebd50ef5
shobhit2603 <shrivastavashobhit706@gmail.com>
2026-01-06 22:49:32 +0530

    Added routes path using react-router-dom

--- frontend/src/landing_page/Navbar.jsx
+++ frontend/src/landing_page/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Menu } from 'lucide-react';
 import { navLinks } from '../constants';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
     
@@ -7,15 +8,15 @@ export default function Navbar() {
         <nav className='fixed top-0 left-0 w-full bg-white z-50'>
             <div className='h-17 flex items-center justify-between px-50 border-b-neutral-200 border-b'>
                 <div>
-                    <a href='/'>
+                    <Link to='/'>
                     <img src='/logo.svg' alt='Logo' className='w-35 cursor-pointer' />
-                    </a>
+                    </Link>
                 </div>
                 <div>
                     <ul className='flex gap-10'>
                         {navLinks.map(({ id, name, href }) => (
                             <li key={id} className='text-neutral-500 text-sm font-medium hover:text-[#387ed1] cursor-pointer'>
-                                <a href={href}>{name}</a>
+                                <Link to={href}>{name}</Link>
                             </li>
                         ))}
                         <li>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import HomePage from './landing_page/home/HomePage'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import HomePage from "./landing_page/home/HomePage";
+import SignUp from "./landing_page/signup/SignUp";
+import AboutPage from "./landing_page/about/AboutPage";
+import ProductsPage from "./landing_page/products/ProductsPage";
+import PricingPage from "./landing_page/pricing/PricingPage";
+import SupportPage from "./landing_page/support/SupportPage";
+import Navbar from "./landing_page/Navbar";
+import Footer from "./landing_page/Footer";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <HomePage />
-  </StrictMode>
-)
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/products" element={<ProductsPage />} />
+      <Route path="/pricing" element={<PricingPage />} />
+      <Route path="/support" element={<SupportPage />} />
+    </Routes>
+    <Footer />
+  </BrowserRouter>
+);
