a15ce43c18e17e289b8151a3993a2bfbe9743d91
shobhit2603 <shrivastavashobhit706@gmail.com>
2026-01-29 15:25:35 +0530

    Created Signup Page

--- frontend/src/landing_page/home/Hero.jsx
+++ frontend/src/landing_page/home/Hero.jsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function Hero() {
     return (
         <section className='p-5 mb-40 mt-20'>
             <div className='flex flex-col items-center text-center'>
-                <img src='/landing.svg' alt='Hero Image' className='my-6 w-200'/>
+                <img src='/landing.svg' alt='Hero Image' className='my-6 w-200' />
                 <h1 className='mt-6 text-3xl font-medium text-neutral-700'>Invest in everything</h1>
                 <p className='mt-3 mb-10 text-xl text-neutral-700'>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
-                <button className='bg-[#387ed1] text-white font-medium text-xl py-2 px-8 rounded cursor-pointer hover:bg-neutral-900'>Sign up for free</button>
+                <Link to="/signup"><button className='bg-[#387ed1] text-white font-medium text-xl py-2 px-8 rounded cursor-pointer hover:bg-neutral-900'>Sign up for free</button></Link>
             </div>
         </section>
     )
